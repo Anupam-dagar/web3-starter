27e92db696ee5204018c2927c9e22ec2442e3c82
Anupam-dagar <dagaranupam@gmail.com>
2022-04-07 22:10:28 +0530

    complete lottery contract frontend.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,26 +1,73 @@
 import "./App.css";
 import React from "react";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: "",
+  };
 
-    this.state = {
-      manager: "",
-    };
-  }
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
     console.log(lottery);
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: "Waiting on transaction success..." });
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+    this.setState({ message: "You have been entered!" });
+  };
+
+  onClick = async (e) => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+
+    this.setState({ message: "A winner has been picked!" });
+  };
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by: {this.state.manager}</p>
+        <p>
+          This contract is managed by: {this.state.manager}.<br />
+          There are currently
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={(e) => this.setState({ value: e.target.value })}
+            />
+            <button>Enter</button>
+          </div>
+        </form>
+        <hr />
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+        <hr />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
