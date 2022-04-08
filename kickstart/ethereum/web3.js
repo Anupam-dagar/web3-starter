1b50d88dd6d98a6990ef310707e10b0058010bc3
Anupam-dagar <dagaranupam@gmail.com>
2022-04-08 23:05:31 +0530

    fetch all campaigns

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,6 +1,17 @@
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
-const web3 = new Web3(window.ethereum);
+let web3;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // browser running metamask
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // server side or metamask not present
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/b26bcb2ce97143138bd27606d822c477"
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
